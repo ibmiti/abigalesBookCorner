21243f57617fdfb8a453b0472dd0cd7d31687f5e
Dimitri M <ibmiti@gmail.com>
2023-09-12 19:05:53 -0500

    another small change: adding try catch

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -26,10 +26,44 @@ class BlogController extends Controller
     | ---------------
     */
     public function index(){
-        $blogPosts = Blog::latest()->paginate(10);
-        return view('blog.index', [
+
+        try {
+            $blogPosts = Blog::latest()->paginate(10);
+            return view('blog.index', [
             'blogPosts' => $blogPosts,
         ]);
+        } catch (\Exception $e){
+            error_log('An exception occurred: ' . $e->getMessage());
+
+            // Add an error flash message to the session
+            session()->flash('error', 'An error occurred: ' . $e->getMessage());
+
+            // Email support email when error occurs
+              // Compose the error message
+            $errorMessage = "Error Message: " . $e->getMessage() . "\n";
+            $errorMessage .= "File: " . $e->getFile() . "\n";
+            $errorMessage .= "Line: " . $e->getLine() . "\n";
+            $errorMessage .= "Trace: " . $e->getTraceAsString();
+
+            // Send an email with the error message
+            $to = "support@ofroot.technology";
+            $subject = "Error Notification: AbigalesBookCorner";
+            $headers = "From: support@ofroot.technology";
+            $message = $errorMessage;
+
+            // Use the mail() function to send the email
+            $mailSent = mail($to, $subject, $message, $headers);
+
+            if ($mailSent) {
+                echo "Error notification email sent successfully.";
+            } else {
+                echo "Failed to send error notification email.";
+            }
+
+            // Redirect back with the error flash message
+            return back();
+        }
+
     }
 
     /**
